01bc401645bd7fd3b371070f9d78b772a4283e2f
Alex Kozak <alex@commoner.(none)>
2009-07-08 14:04:43 -0700

    Re-fixing CC footer with new image

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 
 <?php $current_site = get_current_site(); ?>
+
 <hr />
 <div id="footer">
 <!-- If you'd like to support WordPress, having the "powered by" link someone on your blog is the best way, it's our only promotion or advertising. -->
@@ -16,11 +17,11 @@
 <!-- Gorgeous design by Michael Heilemann - http://binarybonsai.com/kubrick/ -->
 <?php /* "Just what do you think you're doing Dave?" */ ?>
 
-<!-- <a href="<?=licenseUri()?>">
-<img src="<?php bloginfo('stylesheet_directory'); ?>/images/somerights20.png" alt="some rights reserved" /></a><br/>
+<a href="<?=licenseUri()?>">
+<span style="font-size:80%;"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/88x31.png" alt="some rights reserved" /></a><br/>
 Except where otherwise <a href="http://creativecommons.org/policies">noted</a>,
 content on this site is<br/>
-licensed under a <a rel="license" href="<?=licenseUri()?>">Creative Commons License</a>.-->
+licensed under a <a rel="license" href="<?=licenseUri()?>">Creative Commons Attribution United States 3.0 License</a>.</span>
 		<?php wp_footer(); ?>
 </body>
 </html>
