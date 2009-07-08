b95560db7dc4d4320151496ac0bdad7fc0dde464
Alex Kozak <alex@commoner.(none)>
2009-07-08 16:15:52 -0700

    Footer changes

--- footer.php
+++ footer.php
@@ -1,14 +1,10 @@
-
 <?php $current_site = get_current_site(); ?>
-
-<hr />
-<div id="footer">
-<!-- If you'd like to support WordPress, having the "powered by" link someone on your blog is the best way, it's our only promotion or advertising. -->
-	<p>
-		<?php bloginfo('name'); ?> is proudly powered by
-		<a href="http://mu.wordpress.org/">WordPress MU</a> provided by <a href="http://<?php echo $current_site->domain . $current_site->path ?>"><?php echo $current_site->site_name ?></a>. 
-		<br /><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a>
-		and <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.
+-
+ <hr />
+ <div id="footer">
+        <p>
+               <?php bloginfo('name'); ?> is a project of <a href="http://learn.creativecommons.org">ccLearn</a> at <a href="http://creativecommons.org">Creative Commons</a>. 
+		<br /><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a>.
 		<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
 	</p>
 </div>
@@ -18,7 +14,7 @@
 <?php /* "Just what do you think you're doing Dave?" */ ?>
 
 <a href="<?=licenseUri()?>">
-<span style="font-size:80%;"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/88x31.png" alt="some rights reserved" /></a><br/>
+<span style="font-size:80%;"><img src="/images/88x31.png" alt="some rights reserved" /></a><br/>
 Except where otherwise <a href="http://creativecommons.org/policies">noted</a>,
 content on this site is<br/>
 licensed under a <a rel="license" href="<?=licenseUri()?>">Creative Commons Attribution United States 3.0 License</a>.</span>
