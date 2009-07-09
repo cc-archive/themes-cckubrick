b0ff101b36753afe816c60219655c924392774aa
Alex Kozak <alex@commoner.(none)>
2009-07-09 10:07:16 -0700

    Modified the footer, added bg-std.png to the theme instead of pulling from CC

--- footer.php
+++ footer.php
@@ -3,21 +3,17 @@
  <hr />
  <div id="footer">
         <p>
-               <?php bloginfo('name'); ?> is a project of <a href="http://learn.creativecommons.org">ccLearn</a> at <a href="http://creativecommons.org">Creative Commons</a>. 
-		<br /><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a>.
+               <?php bloginfo('name'); ?> is a project of <a href="http://learn.creativecommons.org">ccLearn</a> at <a href="http://creativecommons.org">Creative Commons</a>. <a href="<?php bloginfo('rss2_url'); ?>">Blog RSS</a><br />
 		<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
-	</p>
-</div>
+<a href="<?=licenseUri()?>"></a>
+<span style="font-size:80%;">
+Except where otherwise <a href="http://creativecommons.org/policies">noted</a>, content on this site is
+licensed under a <a rel="license" href="<?#licenseUri()?>">Creative Commons Attribution United States 3.0 License</a></span>
+</p>
 </div>
 
+<?php wp_footer(); ?>
 <!-- Gorgeous design by Michael Heilemann - http://binarybonsai.com/kubrick/ -->
 <?php /* "Just what do you think you're doing Dave?" */ ?>
-
-<a href="<?=licenseUri()?>">
-<span style="font-size:80%;"><img src="http://i.creativecommons.org/l/by/3.0/88x31.png" alt="some rights reserved" /></a><br/>
-Except where otherwise <a href="http://creativecommons.org/policies">noted</a>,
-content on this site is<br/>
-licensed under a <a rel="license" href="<?=licenseUri()?>">Creative Commons Attribution United States 3.0 License</a>.</span>
-		<?php wp_footer(); ?>
 </body>
 </html>
