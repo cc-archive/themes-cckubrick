de021730da1a76501f5329aba398e8dfef17a00a
Alex Kozak <alex@commoner.(none)>
2009-07-09 10:22:27 -0700

    Fixed div problem in main template

--- main.php
+++ main.php
@@ -25,13 +25,13 @@ Template Name: Main
 <?php
 $how_many=5; //How many posts do you want to show
 require_once('wp-config.php'); // Change this for your path to wp-config.php file ?>
-<ol id="whats-new">
+<ul id="whats-new">
 <?
 $news=$wpdb->get_results("SELECT `ID`,`post_title` FROM $wpdb->posts
 WHERE `post_type`=\"post\" AND `post_status`=\"publish\" ORDER BY post_date DESC LIMIT $how_many");
 foreach($news as $np){
 printf ("<li><a href=\"index.php?p=%s\">%s</a></li>", $np->ID,$np->post_title);
 }?>
-</ol>
-
+</ul>
+</div>
 <?php get_footer(); ?>
