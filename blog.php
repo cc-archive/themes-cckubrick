5e6d220ee8f722fa0963ad7b98e22459df9c9ef6
Alex Kozak <alex@commoner.(none)>
2009-07-09 10:24:02 -0700

    Added a new template for the blog page

---
+++ blog.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Template Name: Blog
+*/
+?>
+
+<?php get_header(); ?>
+
+        <div id="content" class="narrowcolumn">
+
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <div class="post" id="post-<?php the_ID(); ?>">
+                <h2><?php the_title(); ?></h2>
+                        <div class="entry">
+                                <?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
+
+                                <?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
+
+                        </div>
+                </div>
+                <?php endwhile; endif; ?>
+        <?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
+
+<h3>Recent Posts:</h3>
+<?php
+$how_many=5; //How many posts do you want to show
+require_once('wp-config.php'); // Change this for your path to wp-config.php file ?>
+<ul id="whats-new">
+<?
+$news=$wpdb->get_results("SELECT `ID`,`post_title` FROM $wpdb->posts
+WHERE `post_type`=\"post\" AND `post_status`=\"publish\" ORDER BY post_date DESC LIMIT $how_many");
+foreach($news as $np){
+printf ("<li><a href=\"index.php?p=%s\">%s</a></li>", $np->ID,$np->post_title);
+}?>
+</ul>
+</div>
+<?php get_footer(); ?>
