5b3e9ba8e108f9d3e4b7e426b35de5a556b406d1
Alex Kozak <alex@commoner.(none)>
2009-07-09 15:51:19 -0700

    Fix divs

--- main.php
+++ main.php
@@ -20,10 +20,9 @@ Template Name: Main
                 </div>
                 <?php endwhile; endif; ?>
         <?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
-</div>
 
 <div style="width: 960px;height: 100px;">
 <?php echo $falbum->show_random(8,'Student Journalism 2.0',1,'s');?>
 </div>
-
+</div>
 <?php get_footer(); ?>
