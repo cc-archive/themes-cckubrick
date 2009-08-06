4ba9158147d30f1c0b5ff47f7b722e995fa391b2
Alex Kozak <alex@commoner.(none)>
2009-08-06 15:38:30 -0700

    Removed dependence on wpLicense for licenseURI, just hard-coded it

--- footer.php
+++ footer.php
@@ -5,10 +5,9 @@
         <p>
                <?php bloginfo('name'); ?> is a project of <a href="http://learn.creativecommons.org">ccLearn</a> at <a href="http://creativecommons.org">Creative Commons</a>. <a href="<?php bloginfo('rss2_url'); ?>">Blog RSS</a><br />
 		<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
-<a href="<?=licenseUri()?>"></a>
-<span style="font-size:80%;">
+<span style="font-size:85%;">
 Except where otherwise <a href="http://creativecommons.org/policies">noted</a>, content on this site is
-licensed under a <a rel="license" href="<?#licenseUri()?>">Creative Commons Attribution 3.0 Unported License</a></span>
+licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 Unported License</a></span>
 </p>
 </div>
 
